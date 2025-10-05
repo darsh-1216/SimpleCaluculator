dd80ed3ce4841e1d8e4a0740057ab0bf581004a3
virendr <virendrkumarprajapati84@gmail.com>
2025-10-05 15:43:40 +0000

    Added Enhanced UI

--- Calculator/script.js
+++ Calculator/script.js
@@ -16,14 +16,23 @@ function clearResult() {
     result.value = '';
 }
 
-
 function backspace() {
     result.value = result.value.slice(0, -1);
 }
 
+const box = document.getElementById("resultBox");
+box.scrollLeft = box.scrollWidth; 
+
+function updateResult(text) {
+    const box = document.getElementById("result");
+    box.textContent = text;
+    box.scrollLeft = box.scrollWidth; 
+}
+
+
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Backspace') {
-        event.preventDefault(); 
+        event.preventDefault();
         backspace();
     }
 });
